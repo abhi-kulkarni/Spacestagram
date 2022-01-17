3c883e73140d972cb8ea07defbf765fe46fcf3d5
ABHISHEK <a29kulka@uwaterloo.ca>
2022-01-16 21:27:49 -0500

    navigation fixes

--- spacestagram/src/App.js
+++ spacestagram/src/App.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, {useEffect} from "react";
 import './App.css';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import IndexPage from "./pages/IndexPage"
 import Gallery from './pages/Gallery';
 import Header from './components/Header';
 import Footer from './components/Footer';
-function App() {
 
+function App() {
+  
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate('/');
+  }, [])
+  
   return (
     <div className="App">
       <Header/>
